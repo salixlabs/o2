df71e7aab064f009bf1831906d604e88a7489630
Alejo Vilches <alejoevilches@gmail.com>
2025-07-31 16:18:27 -0300

    fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   adapter: node({ mode: "standalone" }),
+  output: 'server',
   vite: {
     plugins: [tailwindcss(), mdx()],
   },
