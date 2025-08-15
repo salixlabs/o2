e6d3fe4e488f789dda5df88ff31416b6ef55b0aa
Alejo Vilches <alejoevilches@gmail.com>
2025-08-15 13:02:16 -0300

    fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,12 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://salixlabs.github.io/officetwo/',
-  adapter: node({ mode: "standalone" }),
-  output: 'server',
-  server: {
-    host: true,
-  },
+  base: '/officetwo',
+  output: 'static',
   vite: {
     plugins: [tailwindcss(), mdx()],
   },
