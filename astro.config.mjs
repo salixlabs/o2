bc72afbabcff5b8ae8198cd671f42ad7ec6fdf73
Alejo Vilches <alejoevilches@gmail.com>
2025-08-15 14:12:23 -0300

    changes on astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
+// @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import node from "@astrojs/node";
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://www.officetwo.com',
   output: 'server',
   adapter: node({ mode: 'standalone' }),
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+  },
   vite: {
     plugins: [tailwindcss(), mdx()],
   },
