5b955fcef445ddf4df9d57f76b00400fe094a349
Alejo Vilches <alejoevilches@gmail.com>
2025-08-15 14:01:22 -0300

    prepare render deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://salixlabs.github.io/officetwo/',
-  base: '/officetwo',
+  site: 'https://www.officetwo.com',
+  output: 'server',
+  adapter: node({ mode: 'standalone' }),
   vite: {
     plugins: [tailwindcss(), mdx()],
   },
