3a98b0875ae0a3ec3fc2d8f565eec289baa99f42
Alejo Vilches <alejoevilches@gmail.com>
2025-08-15 12:38:56 -0300

    create ci workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://salixlabs.github.io/officetwo/',
   adapter: node({ mode: "standalone" }),
   output: 'server',
   server: {
